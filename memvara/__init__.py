8bcfb45fd79e314902e36e77a3163f400c93c895
Inderjeet Singh <57476662+sethigoldy@users.noreply.github.com>
2026-09-25 16:20:04 +0530

    Release 0.16.0 (#271)
    Bumps the version to 0.16.0 and closes out the changelog for 2026-09-25.

--- memvara/__init__.py
+++ memvara/__init__.py
@@ -115,7 +115,7 @@ from .write.reconcile import (
     MergeReport, SplitReport, backfill_entities, backfill_predicates, split_entity,
 )
 
-__version__ = "0.15.0"
+__version__ = "0.16.0"
 
 __all__ = [
     "Memvara", "ScopedMemvara", "AsyncMemvara", "AsyncScopedMemvara",
